1b992ef39c4271e1014cc9a43cb920a66372d0e4
felicetto13 <felice.laterza93@icloud.com>
2022-07-01 12:42:29 +0200

    filtered image project

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <nav>
+            <div class="logo-navbar">
+                <img src="../../public/img/" alt="">
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
